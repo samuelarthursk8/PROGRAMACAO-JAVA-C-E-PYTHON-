74331440e2410037d8a2c71338d0b8e9d345e37f
samuelarthursk8 <104212446+samuelarthursk8@users.noreply.github.com>
2022-09-23 09:42:33 -0300

    Add files via upload

---
+++ q5td2.py
@@ -0,0 +1,8 @@
+nota1=float(input("Digite a primeira nota:"))
+nota2=float(input("Digite a segunda nota:"))
+media=(nota1+nota2)/2
+if media>=7:
+    print("Aluno aprovado :)")
+else:
+    print("Aluno reprovado!!")
+print("A media do aluno foi:", media)
